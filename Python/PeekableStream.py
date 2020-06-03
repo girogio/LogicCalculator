a358a5576549f6a73cb8f6fa545a64c20b331873
Juan Scerri <juan.scerri.js@gmail.com>
2020-06-03 22:59:37 +0200

    Removed Unecessary Code

--- Python/PeekableStream.py
+++ Python/PeekableStream.py
@@ -3,7 +3,6 @@ class PeekableStream:
     def __init__(self, stream):
         self.stream = stream
         self.pos = 0
-        self.posStack = []
         self.currentElem = self.stream[self.pos]
 
 
@@ -19,28 +18,6 @@ class PeekableStream:
                 self.currentElem = None
 
         else:
-            raise "End Of Stream."
+            raise "End Of Stream"
 
         return ret
-
-
-    def prevElem(self):
-        ret = self.currentElem
-        self.pos -= 1
-
-        if self.pos >= 0:
-            self.currentElem = self.stream[self.pos]
-        else:
-            self.pos += 1
-            raise "Beginning Of Stream."
-
-        return ret
-
-
-    def pushPos(self):
-        self.posStack.append(self.pos)
-
-    def popPos(self):
-        pos = self.posStack.pop()
-        self.pos = pos
-        self.currentElem = self.stream[self.pos]
