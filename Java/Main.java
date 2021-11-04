271cb6007f2495c030ea760e4bbfdf14f1bec03b
Girogio <giorgiogrigolo03@gmail.com>
2021-11-04 10:54:17 +0100

    Close scanner to avoid buffer leakage.

--- Java/Main.java
+++ Java/Main.java
@@ -12,33 +12,31 @@ public class Main {
         ArrayList<String[]> tokens = new ArrayList<String[]>();
 
         try {
-            tokens = lc.lex(expression);    
-        } catch(Exception e) {
+            tokens = lc.lex(expression);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         // for(int i = 0; i < tokens.size(); i++) {
-        //     System.out.println(tokens.get(i)[0] + ", " + tokens.get(i)[1]);
+        // System.out.println(tokens.get(i)[0] + ", " + tokens.get(i)[1]);
         // }
 
         ArrayList<String[]> rpn = lc.rpn(tokens);
         // System.out.println(q);
 
-        for(int i = 0; i < rpn.size(); i++) {
+        for (int i = 0; i < rpn.size(); i++) {
             System.out.print(rpn.get(i)[0] + ", " + rpn.get(i)[1] + " ");
         }
-
-
 
         boolean[][] table = lc.genTable();
         boolean[] table2 = lc.calcTruthTable(rpn);
         // String[][] stringTable = new String[table.length + 1][table2.length];
         System.out.println();
-            
-        for(int i = 0; i < table[0].length; i++) {
-            for(int j = 0; j < table.length; j++) {
+
+        for (int i = 0; i < table[0].length; i++) {
+            for (int j = 0; j < table.length; j++) {
                 System.out.print(table[j][i] + ", ");
-         //       stringTable[j][i] = String.valueOf(table[j][i]);
+                // stringTable[j][i] = String.valueOf(table[j][i]);
             }
 
             System.out.println();
@@ -46,33 +44,25 @@ public class Main {
 
         System.out.println();
 
-        for(int i = 0; i < table2.length; i++) {
+        for (int i = 0; i < table2.length; i++) {
             System.out.println(table2[i]);
-           // stringTable[table.length][i] = String.valueOf(table2[i]);
+            // stringTable[table.length][i] = String.valueOf(table2[i]);
         }
         System.out.println();
 
-
-       /* for(int i = 0; i < stringTable[0].length; i++){
-            for(int j = 0; j < stringTable.length; j++){
-                System.out.print(stringTable[j][i] + ", ");
-            }
-            System.out.println();
-        }
-
-    String[] headers = new String[stringTable[0].length - 1];
-
-    for(int i = 0; i < headers.length; i++){
-        headers[i] = "-";
+        /*
+         * for(int i = 0; i < stringTable[0].length; i++){ for(int j = 0; j <
+         * stringTable.length; j++){ System.out.print(stringTable[j][i] + ", "); }
+         * System.out.println(); }
+         * 
+         * String[] headers = new String[stringTable[0].length - 1];
+         * 
+         * for(int i = 0; i < headers.length; i++){ headers[i] = "-"; }
+         * 
+         * JFrame f; f = new JFrame(); JTable jt= new JTable(stringTable, headers);
+         * jt.setBounds(30,40,200,300); JScrollPane sp=new JScrollPane(jt); f.add(sp);
+         * f.setSize(300,400); f.setVisible(true);
+         */
+        sc.close();
     }
-    
-    JFrame f;
-    f = new JFrame();
-    JTable jt= new JTable(stringTable, headers);
-    jt.setBounds(30,40,200,300);          
-    JScrollPane sp=new JScrollPane(jt);    
-    f.add(sp);          
-    f.setSize(300,400);    
-    f.setVisible(true);   
-*/}
 }
